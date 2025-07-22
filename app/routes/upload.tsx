1f5fb25b5e23c984524251c272c58ce704539282
SnebYT <86675246+SnebYT@users.noreply.github.com>
2025-07-22 18:53:23 +0530

    Resume feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -5,9 +5,11 @@ import {usePuterStore} from "~/lib/puter";
 import {convertPdfToImage} from "~/lib/pdf2img";
 import {generateUUID} from "~/lib/utils";
 import {prepareInstructions} from "../../constants";
+import {useNavigate} from "react-router";
 
 const Upload = () => {
     const {auth, isLoading, fs, ai, kv} = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
     const [file, setFile] = useState<File | null>(null)
@@ -63,6 +65,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis Complete, redirecting...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
