aef11fc265811cafa0d61ce292de43034007bf0c
SnebYT <86675246+SnebYT@users.noreply.github.com>
2025-07-23 10:43:12 +0530

    added wipe functionality for testing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -47,7 +47,6 @@ export default function Home() {
         {!loadingResumes && resumes?.length === 0? (
             <h2>No resumes found. Upload your first resume to get feedback.</h2>
         ): (<h2>Review your submissions and check AI-powered feedback.</h2>)}
-        <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
 
       {loadingResumes && (
